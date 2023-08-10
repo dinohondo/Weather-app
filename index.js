590b08739cfd79eaae60d8cde9eb73fd200d2083
Bilal Softic <bilalsoftic99@gmail.com>
2023-08-10 19:56:44 +0200

    current location

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello');
+/* 1. my location */
+const locationBtn = document.querySelector('#my-location');
+const locationDisplay = document.querySelector('#location-display');
+
+locationBtn.addEventListener('click', function () {
+  try {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lat = `${position.coords.latitude}`;
+      const lon = `${position.coords.longitude}`;
+    });
+  } catch (error) {
+    locationDisplay.innerHTML = 'Error getting location.';
+  }
+});
+/* 2. search location */
+/* 3. display  weather*/
